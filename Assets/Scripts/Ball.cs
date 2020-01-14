0f11a7a7e9cf95dbc8f44460a778f94d82e7c849
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-01-14 17:41:08 +0000

    Lock ball to paddle and launch with mouse click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    Vector2 paddleToBallDistance;
+    bool hasLaunched = false;
+
+    void Start()
+    {
+        paddleToBallDistance = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        if (!hasLaunched)
+        {
+            LockToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LockToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallDistance;
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasLaunched = true;
+        }
+    }
+}
